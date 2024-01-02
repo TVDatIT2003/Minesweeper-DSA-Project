8579efb50cdaec96e9207e94e68dfcc06afe5cca
Duc Anh Nguyen <92425301+ducknguyene@users.noreply.github.com>
2024-01-02 10:44:23 +0700

    Update

--- src/src/CheckSquare.java
+++ src/src/CheckSquare.java
@@ -1,6 +1,14 @@
+/* Name: Group 6
 
+   Member names & IU code
+    Trần Văn Đạt - ITITIU21173
+    Nguyễn Phạm Đức Anh - ITITIU21033
+    Phạm Thế Thiện - ITDSIU20084
+    Trịnh Tiến Đạt - ITDSIU20109
+
+    Purpose: Minesweeper, a puzzle game about uncovering safe areas while avoiding hidden mines. It's a test of logic and quick decision-making.
+*/
 /**
-
  * This class provides a way to count the total number of bombs which surrounds the given square.
  */
 public class CheckSquare
