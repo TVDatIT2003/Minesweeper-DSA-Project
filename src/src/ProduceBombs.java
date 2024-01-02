4450cf91bce8d441d028437057d38858a0be0dbe
Duc Anh Nguyen <92425301+ducknguyene@users.noreply.github.com>
2024-01-02 10:48:19 +0700

    Update

--- src/src/ProduceBombs.java
+++ src/src/ProduceBombs.java
@@ -1,5 +1,13 @@
+/* Name: Group 6
 
+   Member names & IU code
+    Trần Văn Đạt - ITITIU21173
+    Nguyễn Phạm Đức Anh - ITITIU21033
+    Phạm Thế Thiện - ITDSIU20084
+    Trịnh Tiến Đạt - ITDSIU20109
 
+    Purpose: Minesweeper, a puzzle game about uncovering safe areas while avoiding hidden mines. It's a test of logic and quick decision-making.
+*/
 
 import java.util.Random;
 
